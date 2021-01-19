a0782284ae93d32b18a5a802ed462ab53f57f6bc
Bartłomiej Mont <spankertroll77@gmail.com>
2021-01-19 10:42:56 +0100

    Initial commit

---
+++ src/main/java/com/bartmont/VetClinicREST/VetClinicRestApplication.java
@@ -0,0 +1,13 @@
+package com.bartmont.VetClinicREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetClinicRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetClinicRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bartmont/VetClinicREST/VetClinicRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bartmont.VetClinicREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetClinicRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
